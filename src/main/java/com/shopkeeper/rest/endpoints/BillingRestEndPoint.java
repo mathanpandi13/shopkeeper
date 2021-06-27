1ac3aaeb98a0369a052fbb6269195e84093cb660
ef6mrs <ef6mrs@host.docker.internal>
2021-06-27 18:59:58 +0530

    Updated REST API defnitions

--- src/main/java/com/shopkeeper/rest/endpoints/BillingRestEndPoint.java
+++ src/main/java/com/shopkeeper/rest/endpoints/BillingRestEndPoint.java
@@ -1,27 +1,30 @@
 package com.shopkeeper.rest.endpoints;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.shopkeeper.rest.beans.AddBillRequest;
+import com.shopkeeper.rest.endpoints.constants.BillingEndPointConstants;
 
 /**
  * Rest APIs related to Billing.
- * @author ef6mrs
+ * @author ef6mrs mathan
  *
  */
-@RequestMapping("/billing")
+@RequestMapping(BillingEndPointConstants.BILLING)
 @RestController
 public class BillingRestEndPoint {
 
-	@GetMapping(value = "/bill{billId}")
-	public String getBill() {
+	@GetMapping(value = BillingEndPointConstants.BILL + "/{billId}")
+	public String getBill(@PathVariable String billId) {
 		return "bill";
 	}
 
-	@PostMapping(value="/bill")
-	public void addBill(AddBillRequest addBillRequest) {
+	@PostMapping(value= BillingEndPointConstants.BILL)
+	public void addBill(@RequestBody AddBillRequest addBillRequest) {
 	}
 }
--- src/main/java/com/shopkeeper/rest/endpoints/constants/BillingEndPointConstants.java
+++ src/main/java/com/shopkeeper/rest/endpoints/constants/BillingEndPointConstants.java
@@ -1,5 +1,19 @@
 package com.shopkeeper.rest.endpoints.constants;
 
+/**
+ * Constants for rest end points.
+ * @author ef6mrs mathan
+ *
+ */
 public class BillingEndPointConstants {
 
+	/**
+	 * Request mapping for billing APIs.
+	 */
+	public final static String BILLING = "/billing";
+	
+	/**
+	 * Mapping for bill REST APIs.
+	 */
+	public final static String BILL = "/bill";
 }
